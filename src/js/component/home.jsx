dd5448b1e5b9a51550c25c6f41b6b2bdb97d21d0
joan <joanflores905@gmail.com>
2022-10-12 21:08:18 +0000

    restructuracion de codigo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+    return (
+        <div className="text-center d-flex justify-content-center">
+            <div className="bigCounter d-flex fs-1 gap-4 text-light bg-black w-25 justify-content-center">
+                <div className="Clock ps-5"><i className="fa fa-clock"></i></div>
+                <div className="Seis">{props.digitSix % 10}</div>
+                <div className="Cinco">{props.digitFive % 10}</div>
+                <div className="Cuatro">{props.digitFour % 10}</div>
+                <div className="Tres">{props.digitThree % 10}</div>
+                <div className="Dos">{props.digitTwo % 10}</div>
+                <div className="Uno pe-5">{props.digitOne % 10}</div>
+            </div>
+        </div>
+    );
 };
-
+Home.propTypes = {
+    digitOne: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitSix: PropTypes.number,
+}
 export default Home;
